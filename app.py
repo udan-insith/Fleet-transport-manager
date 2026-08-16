04d154ebb0f540f517d17d5595cf2e9d51d4e361
udan-insith <udaninsith23613@gmail.com>
2026-08-16 11:53:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,3 +10,10 @@ import database
 import excel_sync
 import auth
 import utils
+
+st.set_page_config(
+    page_title="BOC Transport Management System",
+    page_icon="🚌",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
