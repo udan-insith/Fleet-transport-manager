a144bda6996537be60f606bf6332fd94c6462180
Udan Insith <udaninsith23613@gmail.com>
2026-08-28 08:05:49 +0530

    One time initialization

--- boc_tms/app.py
+++ boc_tms/app.py
@@ -10,3 +10,19 @@ import database
 import excel_sync
 import auth
 import utils
+
+# PAGE CONFIG + ONE TIME INITIALIZATION
+st.set_page_config(
+    page_title="BOC Transport Management System",
+    page_icon="🚌",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+ 
+database.init_db()
+database.seed_if_empty()
+ 
+if "backup_synced" not in st.session_state:
+    excel_sync.trigger_backup()
+    st.session_state["backup_synced"] = True
+ 
